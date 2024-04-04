627cafa7474b6351760ca34bf204ffd2e1d3b103
RomanSzabo <37343179+RomanSzabo@users.noreply.github.com>
2024-04-04 15:42:05 +0200

    Update config.js
    adding base url

--- src/config.js
+++ src/config.js
@@ -15,6 +15,7 @@ class Config {
       iamRoleName: core.getInput('iam-role-name'),
       runnerHomeDir: core.getInput('runner-home-dir'),
       preRunnerScript: core.getInput('pre-runner-script'),
+      baseUrl: core.getInput('base-url')
     };
 
     const tags = JSON.parse(core.getInput('aws-resource-tags'));
